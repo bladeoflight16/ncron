f4f084faf6c0bbc5e8a175252eb99c28bcec21f1
bladeoflight16 <bladeoflight16@gmail.com>
2017-08-03 14:22:11 -0400

    Support loading ApplicationName from a config value

--- src/NCron/ApplicationInfo.cs
+++ src/NCron/ApplicationInfo.cs
@@ -1,9 +1,28 @@
-﻿using System.Reflection;
+﻿using System.Configuration;
+using System.Reflection;
+using System.Linq;
 
 namespace NCron
 {
     internal static class ApplicationInfo
     {
-        public static readonly string ApplicationName = (Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly()).GetName().Name;
+        private const string NameKey = "NCronAppName";
+
+        public static readonly string ApplicationName;
+
+        static ApplicationInfo()
+        {
+            if (
+                ConfigurationManager.AppSettings.AllKeys.Contains(NameKey)
+                && !string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings[NameKey])
+            )
+            {
+                ApplicationName = ConfigurationManager.AppSettings[NameKey];
+            }
+            else
+            {
+                ApplicationName = (Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly()).GetName().Name;
+            }
+        }
     }
 }
